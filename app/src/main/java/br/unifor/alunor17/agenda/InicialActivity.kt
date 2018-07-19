41f475a0117682f0e52e4771dc33d93bd6938ecd
alunor17 <alunor17@M72452.local>
2018-07-19 14:04:30 -0300

    Initial commit

---
+++ app/src/main/java/br/unifor/alunor17/agenda/InicialActivity.kt
@@ -0,0 +1,12 @@
+package br.unifor.alunor17.agenda
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class InicialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicial)
+    }
+}
